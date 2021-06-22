dd8b24efb6e00bc9f3d47c145a4f510305b2d3b4
nohsangwoo <nsgr12@naver.com>
2021-06-22 14:07:06 +0900

    connect

--- README.md
+++ README.md
@@ -1,46 +1,9 @@
-# Getting Started with Create React App
+# typescript setting template
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- npx create-react-app "생성할앱이름" --template typescript
 
-## Available Scripts
+# 깊은 복사와 얕은 복사
 
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- {},[] 의 복사시 얕은복사(참조) 와 깊은복사(참조 끊음) 이 존재한다 Like a pointer in C
+- JSON.parse(JSON.stringify("OBJECT")); 가 있긴한데 속도가 넘 느려서 쓰면안됨
+- splice()는 1차깊이의 깊은 복사가 가능함, but 배열안에 오브젝트가 존재한다던지 더 깊은 단계의 obj가 있다면 얕은 복사가됨
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable */
+import { relative } from 'path';
+import React, { useState } from 'react';
+import { convertToObject } from 'typescript';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  let [contents, setContents] = useState([
+    { title: '남자 코트 추천', likes: 0 },
+    { title: '강남 우동 맛집', likes: 1 },
+    { title: '여자 코트 추천', likes: 2 },
+    { title: '강남 짜장 맛집', likes: 3 },
+  ]);
+
+  const copyObj = (obj: any) => {
+    var copy: any = {};
+    if (typeof obj === 'object' && obj !== null) {
+      for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) {
+          copy[attr] = copyObj(obj[attr]);
+        }
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button
+        onClick={() => {
+          setContents(prev => {
+            const obj = { a: 0, b: 1, c: 2, d: 3 };
+            const obj2 = { a: 'one', b: 'two', c: 'three' };
+            const totla = { ...obj, ...obj2 };
+            console.log(totla);
+
+            console.log(prev);
+            let temp = [...prev];
+            temp[0].title = '여자 코트 추천';
+            return temp;
+          });
+        }}
+      >
+        button
+      </button>
+      <div>
+        {contents.map((data, index) => {
+          return (
+            <div className="list" key={index}>
+              <div className="title">
+                <h3 className="align">{data.title}</h3>
+                <span
+                  className="align likesBtn"
+                  onClick={() => {
+                    return setContents(prev => {
+                      let copied: any = [];
+
+                      // 깊은복사 시작
+                      prev.forEach((data: any, index) => {
+                        // 깊은복사 포인트
+                        // copied[index] = data; // 이건 또 얕은 복사가됨
+                        copied[index] = { ...data };
+                      });
+                      copied[index].likes += 1;
+                      const result = [...copied];
+                      return result;
+                    });
+                  }}
+                >
+                  👍
+                </span>
+                <div className="align">{data.likes}</div>
+              </div>
+
+              <p>2월 17일 발</p>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
